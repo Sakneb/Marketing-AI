3e0013d6939b792ca27387109f2c57548249e1d7
Sakina <sakinaibrahimova7@gmail.com>
2024-02-19 20:03:33 +0100

    Build: Homepage

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const About = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}></div>
+      <Image src="/about.png" alt="" fill />
+    </div>
+  );
 };
 
 export default About;
